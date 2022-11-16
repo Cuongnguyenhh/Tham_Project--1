208d7d2e15186bd0d3d14a4ab38a317062542800
Cuongnguyenhh <phucuong200@gmail.com>
2022-11-17 01:43:20 +0700

    fix-index_1

--- doc/index.php
+++ doc/index.php
@@ -1,22 +1,31 @@
 <?php  
-require_once './navbar.php';
 if(isset($_GET['act'])){
     $act =$_GET['act'];
     switch($_GET['act']){
         case 'main':
             require_once './mainControll.php';
         case 'pos':
+            require_once './navbar.php';
             require_once './phan-mem-ban-hang.php';
         case 'ql_nhanvien':
+            require_once './navbar.php';
             require_once './table-data-table.php';
         case 'ql_khachhang':
+            require_once './navbar.php';
             require_once './Customer_control.php';
         case 'product':
+            require_once './navbar.php';
             require_once './table-data-product.php';
         case 'order':
+            require_once './navbar.php';
             require_once './table-data-oder.php';
+        case 'report':
+            require_once './phan-mem-ban-hang.php';
             
     }
+}else{
+    require_once './mainControll.php';
+    require_once './navbar.php';
 }
 
 ?>
